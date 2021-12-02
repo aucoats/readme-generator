2352a90375f76d0c055382911639203b9970e784
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-02 14:45:46 -0500

    initial array, promise chain

--- index.js
+++ index.js
@@ -1,13 +1,109 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require ('inquirer');
+ 
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter your project's title.")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter a brief description of your project. (Required)",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter a description.")
+                return false;
+            }
+        }
+
+    }, 
+    {
+        type: 'input', 
+        name: 'install',
+        message: 'Enter installation instructions if there are any.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Enter usage information.")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines. (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log("Contribution info is required.")
+                return false;
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'Enter any test instructions.'
+    },
+    
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(info) {
+    
+
+    return new Promise((resolve, reject) => { 
+        fs.writeFile(`${info.title}.md`, info, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(data) {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(data => {
+        return generateMarkdown(data)
+    })
+    .then(writeFile => {
+        return writeToFile(writeFile)
+    }).then(writeFileResponse => {
+        console.log(writeFileResponse)
+    })
